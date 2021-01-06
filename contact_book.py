8246a43e3c26e2aef149ca356f128661e0f676a8
Prospero <60527636+Prosperos-Masq@users.noreply.github.com>
2021-01-06 02:08:27 -0600

    Update contact_book.py
    Finished the delete option

--- contact_book.py
+++ contact_book.py
@@ -1,25 +1,21 @@
 contacts = {}
 
+
 while True:
     ask = input("Would you like to access your contact list (y/n)?: ").lower().strip()
     if ask == "y":
         print("Okay!")
-        see = input("Would you like to 1 = see contacts, 2 = add a contact?: ").strip()
+        see = input("Would you like to 1 = see contacts, 2 = add a contact, 3 = delete a contact?: ").strip()
         if see == "1":
             print(contacts)
         elif see == "2":
             name = input("Please enter the contact name: ").capitalize().strip()
             number = int(input("Please add a phone number: "))
-            email = input("Would you like to add a email address (y/n)?: ").lower().strip()
-            if email == "y":
-                add_email = input("Enter a email address: ").strip()
-            else:
-                print("Moving on!")
-            contacts[name] = number, add_email
-       # elif see == "3":
-        #    delete = input("Enter the contact name you would like to delete?: ").strip().lower
-        #    if delete == contacts[name]:
-        #        del contacts[name]
-         #       print(contacts)
+            contacts[name] = number
+        elif see == "3":
+            delete = input("Enter the contact name you would like to delete?: ").strip().capitalize()
+            if delete in contacts:
+                del contacts[delete]
+                print(contacts)
     else:
         print("Alright, maybe another time!")
